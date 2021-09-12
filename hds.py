6a5114e533006a458b52c70cbbacdd4a5d14d3bd
enrique r grullon <e@co8.com>
2021-09-12 09:13:01 -0400

    api sync bold

--- hds.py
+++ hds.py
@@ -692,7 +692,7 @@ def load_hotspot_data_and_status():
 
     # Sync Display
     hs["api_sync"] = (
-        "**" + f"{hs['api_sync']}**" if bool(new_api_sync) else hs["api_sync"]
+        "**" + hs["api_sync"] + "**" if bool(new_api_sync) else hs["api_sync"]
     )
 
     ########################################################
